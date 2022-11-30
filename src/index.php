37f3c4064e4ec86047ecbf52dae70b495675bf62
Gijs Lamon <info@gileba.be>
2022-11-30 15:39:44 +0100

    Starting from scratch

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php
+	defined('_JEXEC') or die('Restricted access');
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" >
+   <head>
+		<jdoc:include type="metas" />
+		<jdoc:include type="styles" />
+		<jdoc:include type="scripts" />
+
+<?php
+		// Set viewport
+		$this->setMetaData('viewport', 'width=device-width, initial-scale=1');
+?>
+	</head>
+	<body>
+	</body>
+</html>
