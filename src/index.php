1dcdecac7c75c0b2919d0f65727b94a1a035906d
Gijs Lamon <info@gileba.be>
2023-02-26 10:16:57 +0000

    Homepagina + navigatie

--- src/index.php
+++ src/index.php
@@ -1,8 +1,19 @@
 <?php
 	defined('_JEXEC') or die('Restricted access');
 
+	use Joomla\CMS\Factory;
+
+	$app = Factory::getApplication();
+	$menu = $app->getMenu();
+	$lang = $app->getLanguage();
 	$wa = $this->getWebAssetManager();
-	$wa->useStyle('template.musalie.base');
+
+	if ($menu->getActive() == $menu->getDefault($lang->getTag())) {
+		$wa->useStyle('template.musalie.home');
+	}
+	else {
+		$wa->useStyle('template.musalie.base');
+	}
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +33,20 @@
 			<div class="logo">
 				<a href="" alt="Home"><img src="media/templates/site/musalie/images/musalie.jpg" alt="Logo" /></a>
 			</div>
+			<nav>
+				<jdoc:include type="modules" name="navigation" style="html5" />
+			</nav>
 		</header>
+		<main>
+<?php	if ($menu->getActive() == $menu->getDefault($lang->getTag())) { ?>
+			<div class="home">
+				<jdoc:include type="modules" name="home" style="html5" />
+			</div>
+<?php	}	?>
+			<jdoc:include type="component" />
+		</main>
 		<footer>
-			<jdoc:include type="modules" name="footer" />
+			<jdoc:include type="modules" name="footer" style="html5" />
 		</footer>
 	</body>
 </html>
